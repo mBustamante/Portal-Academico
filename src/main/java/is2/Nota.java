799cc064388b0dd40733be963955c005d327220b
José Enrique Carrillo Pino <jose.carrillo@ucsp.edu.pe>
2013-10-11 20:35:48 -0500

    Agregando secuencia a matricula y nota

--- src/main/java/is2/Nota.java
+++ src/main/java/is2/Nota.java
@@ -2,13 +2,18 @@ package is2;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
 
 @Entity
 public class Nota {
 
 	@Id
+	@SequenceGenerator(name = "nota_id_gen", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "nota_id_gen")
 	private Long id;
 	
 	private String nombre;
