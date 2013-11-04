8165c66fd20236e627c26b76020f3193b796f6e6
tephy <tephy@tephy-desktop.(none)>
2013-11-04 13:16:03 -0500

    issues #66 scritp insert categoria docente

---
+++ src/sql/docente.sql
@@ -0,0 +1,21 @@
+﻿INSERT INTO docente(
+            id, apellido, fecha_n, nombre, password, telefono, usuario)
+    VALUES (1101,'Vasquez','10/01/1985','Luz Marina','vql1001',952365478,'luzmarina.vasquez');
+INSERT INTO docente(
+            id, apellido, fecha_n, nombre, password, telefono, usuario)
+    VALUES (1102,'Arias','23/06/1988','Diego','apd5234',958536147,'diego.arias');
+INSERT INTO docente(
+            id, apellido, fecha_n, nombre, password, telefono, usuario)
+    VALUES (1103,'Medina','02/012/1987','Tatiana','mtt1895',958539672,'tatiana.medina');
+INSERT INTO docente(
+            id, apellido, fecha_n, nombre, password, telefono, usuario)
+    VALUES (1104,'Basurco','10/01/1985','Luis Fernando','dbl1265',93016782,'luis.basurco');
+INSERT INTO docente(
+            id, apellido, fecha_n, nombre, password, telefono, usuario)
+    VALUES (1105,'Linares','23/05/1985','Rubel','ldr5364',98236100,'rubel.linares');
+INSERT INTO docente(
+            id, apellido, fecha_n, nombre, password, telefono, usuario)
+    VALUES (1106,'Jordan','14/11/1984','Monica','jm1263',975230010,'monica.jordan');
+INSERT INTO docente(
+            id, apellido, fecha_n, nombre, password, telefono, usuario)
+    VALUES (1107,'Tejada','26/09/1982','Javier','tjk4203',954910234,'javier.tejada');
