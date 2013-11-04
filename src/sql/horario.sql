b3aae87e748eedd340b761b14b460fc233aa02c0
tephy <tephy@tephy-desktop.(none)>
2013-11-04 13:31:57 -0500

    issues #66 scritp insert categoria horario

---
+++ src/sql/horario.sql
@@ -0,0 +1,30 @@
+﻿INSERT INTO horario(
+            id, dia, hora_fin, hora_inicio)
+    VALUES (1301,'Lunes',830, 7);
+INSERT INTO horario(
+            id, dia, hora_fin, hora_inicio)
+    VALUES (1302,'Martes',830, 7);
+INSERT INTO horario(
+            id, dia, hora_fin, hora_inicio)
+    VALUES (1303,'Lunes',145, 315);
+INSERT INTO horario(
+            id, dia, hora_fin, hora_inicio)
+    VALUES (1304,'Miercoles',4, 530);
+INSERT INTO horario(
+            id, dia, hora_fin, hora_inicio)
+    VALUES (1305,'Jueves',19,2030);
+INSERT INTO horario(
+            id, dia, hora_fin, hora_inicio)
+    VALUES (1306,'Viernes',1975,2115);
+INSERT INTO horario(
+            id, dia, hora_fin, hora_inicio)
+    VALUES (1307,'Viernes',1815, 1945);
+INSERT INTO horario(
+            id, dia, hora_fin, hora_inicio)
+    VALUES (1308,'Jueves',1430,16);
+INSERT INTO horario(
+            id, dia, hora_fin, hora_inicio)
+    VALUES (1309,'Miercoles',1515,1645);
+INSERT INTO horario(
+            id, dia, hora_fin, hora_inicio)
+    VALUES (1310,'Martes',445,1815);
