054381de8c9843ba3b2ec489c6c8243220020a40
tephy <tephy@tephy-desktop.(none)>
2013-11-04 13:14:32 -0500

    issues #66 scritp insert categoria curso

---
+++ src/sql/curso.sql
@@ -0,0 +1,30 @@
+﻿INSERT INTO curso(
+            id, creditos, idequivalencia, nombre)
+    VALUES (2001, 2, 200101,'Introduccion a la vida universitaria');
+INSERT INTO curso(
+            id, creditos, idequivalencia, nombre)
+    VALUES (2002, 4, 200103,'Logica');
+INSERT INTO curso(
+            id, creditos, idequivalencia, nombre)
+    VALUES (2003, 2,200102,'Formacion cristiana');
+INSERT INTO curso(
+            id, creditos, idequivalencia, nombre)
+    VALUES (2004, 2, 200104,'Musica');
+INSERT INTO curso(
+            id, creditos, idequivalencia, nombre)
+    VALUES (2005, 5, 200105,'Analisis Matematico');
+INSERT INTO curso(
+            id, creditos, idequivalencia, nombre)
+    VALUES (2006, 4, 200105,'Calculo I');
+INSERT INTO curso(
+            id, creditos, idequivalencia, nombre)
+    VALUES (2007, 2, 200102,'Teologia I');
+INSERT INTO curso(
+            id, creditos, idequivalencia, nombre)
+    VALUES (2008,5, 200106,'Fisica I');
+INSERT INTO curso(
+            id, creditos, idequivalencia, nombre)
+    VALUES (2009,4, 200107,'Base de Datos I');
+INSERT INTO curso(
+            id, creditos, idequivalencia, nombre)
+    VALUES (2010, 5, 200108,'Base de Datos II');
