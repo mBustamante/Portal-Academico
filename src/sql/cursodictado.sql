e952d75108b4f609f630dd64c976548a23a5c592
tephy <tephy@tephy-desktop.(none)>
2013-11-04 13:09:12 -0500

    issues #66 scritp insert categoria cursodictado

---
+++ src/sql/cursodictado.sql
@@ -0,0 +1,30 @@
+﻿INSERT INTO cursodictado(
+            id, curso_id, periodo_id)
+    VALUES (4001,2001,3001);
+INSERT INTO cursodictado(
+            id, curso_id, periodo_id)
+    VALUES (4002,2002,3005);
+INSERT INTO cursodictado(
+            id, curso_id, periodo_id)
+    VALUES (4003,2004,3007);
+INSERT INTO cursodictado(
+            id, curso_id, periodo_id)
+    VALUES (4004,2010,3003);
+INSERT INTO cursodictado(
+            id, curso_id, periodo_id)
+    VALUES (4005,2008,3004);
+INSERT INTO cursodictado(
+            id, curso_id, periodo_id)
+    VALUES (4006,2006,3009);
+INSERT INTO cursodictado(
+            id, curso_id, periodo_id)
+    VALUES (4007,2007,3002);
+INSERT INTO cursodictado(
+            id, curso_id, periodo_id)
+    VALUES (4008,2003,3006);
+INSERT INTO cursodictado(
+            id, curso_id, periodo_id)
+    VALUES (4009,2005,3008);
+INSERT INTO cursodictado(
+            id, curso_id, periodo_id)
+    VALUES (4010,2009,3010);
