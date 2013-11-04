06b0463fd501f677d9b529ee43495f03208193e4
tephy <tephy@tephy-desktop.(none)>
2013-11-04 13:12:21 -0500

    issues #66 scritp insert categoria cursomalla

---
+++ src/sql/cursomalla.sql
@@ -0,0 +1,77 @@
+﻿INSERT INTO cursomalla(
+            id, curso_id, malla_id)
+    VALUES (6001, 2001, 7001);
+INSERT INTO cursomalla(
+            id, curso_id, malla_id)
+    VALUES (6002, 2001, 7002);
+INSERT INTO cursomalla(
+            id, curso_id, malla_id)
+    VALUES (6003, 2001, 7003);
+INSERT INTO cursomalla(
+            id, curso_id, malla_id)
+    VALUES (6004, 2001, 7004);
+INSERT INTO cursomalla(
+            id, curso_id, malla_id)
+    VALUES (6005, 2001, 7005);
+INSERT INTO cursomalla(
+            id, curso_id, malla_id)
+    VALUES (6006, 2002, 7006);
+INSERT INTO cursomalla(
+            id, curso_id, malla_id)
+    VALUES (6007, 2003, 7007);
+INSERT INTO cursomalla(
+            id, curso_id, malla_id)
+    VALUES (6008, 2003, 7008);
+INSERT INTO cursomalla(
+            id, curso_id, malla_id)
+    VALUES (6009, 2003, 7009);
+INSERT INTO cursomalla(
+            id, curso_id, malla_id)
+    VALUES (6010, 2003, 7010);
+INSERT INTO cursomalla(
+            id, curso_id, malla_id)
+    VALUES (6011, 2003, 7011);
+INSERT INTO cursomalla(
+            id, curso_id, malla_id)
+    VALUES (6012, 2004, 70012);
+INSERT INTO cursomalla(
+            id, curso_id, malla_id)
+    VALUES (6013, 2004, 70013);
+INSERT INTO cursomalla(
+            id, curso_id, malla_id)
+    VALUES (6014, 2004, 70014);
+INSERT INTO cursomalla(
+            id, curso_id, malla_id)
+    VALUES (6015, 2004, 70015);
+INSERT INTO cursomalla(
+            id, curso_id, malla_id)
+    VALUES (6016, 2005, 70016);
+INSERT INTO cursomalla(
+            id, curso_id, malla_id)
+    VALUES (6017, 2006, 70017);
+INSERT INTO cursomalla(
+            id, curso_id, malla_id)
+    VALUES (6018, 2007, 70018);
+INSERT INTO cursomalla(
+            id, curso_id, malla_id)
+    VALUES (6019, 2007, 70019);
+INSERT INTO cursomalla(
+            id, curso_id, malla_id)
+    VALUES (6020, 2007, 70020);
+INSERT INTO cursomalla(
+            id, curso_id, malla_id)
+    VALUES (6021, 2007, 70021);
+
+INSERT INTO cursomalla(
+            id, curso_id, malla_id)
+    VALUES (6022, 2008, 70022);
+
+INSERT INTO cursomalla(
+            id, curso_id, malla_id)
+    VALUES (6023, 2008, 70025);
+INSERT INTO cursomalla(
+            id, curso_id, malla_id)
+    VALUES (6024, 2009, 70023);
+INSERT INTO cursomalla(
+            id, curso_id, malla_id)
+    VALUES (6025, 2010, 70024);
