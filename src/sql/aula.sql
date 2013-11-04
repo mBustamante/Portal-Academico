203e9486fed2c9ffc4193814a0a803b82083df6d
tephy <tephy@tephy-desktop.(none)>
2013-11-04 13:05:55 -0500

    issues #66 scritp insert aula

---
+++ src/sql/aula.sql
@@ -0,0 +1,30 @@
+﻿INSERT INTO aula(
+            id, maxalumnos, nombre)
+    VALUES (8001,30,'Introduccion a la vida universitaria');
+INSERT INTO aula(
+            id, maxalumnos, nombre)
+    VALUES (8002,20,'Logica');
+INSERT INTO aula(
+            id, maxalumnos, nombre)
+    VALUES (8003,30,'Formacion cristiana');
+INSERT INTO aula(
+            id, maxalumnos, nombre)
+    VALUES (8004,15,'Musica');
+INSERT INTO aula(
+            id, maxalumnos, nombre)
+    VALUES (8005,25,'Analisis Matematico');
+INSERT INTO aula(
+            id, maxalumnos, nombre)
+    VALUES (8006,33,'Calculo I');
+INSERT INTO aula(
+            id, maxalumnos, nombre)
+    VALUES (8007,10,'Teologia I');
+INSERT INTO aula(
+            id, maxalumnos, nombre)
+    VALUES (8008,12,'Fisica');
+INSERT INTO aula(
+            id, maxalumnos, nombre)
+    VALUES (8009,25,'Base de Datos I');
+INSERT INTO aula(
+            id, maxalumnos, nombre)
+    VALUES (8010,25,'Base de Datos II');
