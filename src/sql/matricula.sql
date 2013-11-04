33ce32a43d6ea82bc71098cc36317cc5fe98de78
tephy <tephy@tephy-desktop.(none)>
2013-11-04 13:37:50 -0500

    issues #66 scritp insert categoria matricula

---
+++ src/sql/matricula.sql
@@ -0,0 +1,37 @@
+﻿INSERT INTO matricula(
+            id, estado, fecha, alumno_id, cursodictado_id)
+    VALUES (1601,0,'20/02/13',1001,4002);
+INSERT INTO matricula(
+            id, estado, fecha, alumno_id, cursodictado_id)
+    VALUES (1602,0,'23/02/10',1002,4006);
+INSERT INTO matricula(
+            id, estado, fecha, alumno_id, cursodictado_id)
+    VALUES (1603,0,'20/08/11',1003,4003);
+INSERT INTO matricula(
+            id, estado, fecha, alumno_id, cursodictado_id)
+    VALUES (1604,0,'25/02/12',1004,4008);
+INSERT INTO matricula(
+            id, estado, fecha, alumno_id, cursodictado_id)
+    VALUES (1605,0,'23/08/13',1005,4008);
+INSERT INTO matricula(
+            id, estado, fecha, alumno_id, cursodictado_id)
+    VALUES (1606,0,'22/02/12',1006,4007);
+INSERT INTO matricula(
+            id, estado, fecha, alumno_id, cursodictado_id)
+    VALUES (1607,0,'24/08/11',1007,4010);
+INSERT INTO matricula(
+            id, estado, fecha, alumno_id, cursodictado_id)
+    VALUES (1608,0,'25/02/13',1008,4001);
+INSERT INTO matricula(
+            id, estado, fecha, alumno_id, cursodictado_id)
+    VALUES (1609,0,'25/02/13',1008,4003);
+INSERT INTO matricula(
+            id, estado, fecha, alumno_id, cursodictado_id)
+    VALUES (1610,0,'21/08/12',1009,4007);
+INSERT INTO matricula(
+            id, estado, fecha, alumno_id, cursodictado_id)
+    VALUES (1611,0,'20/08/10',1010,4001);
+INSERT INTO matricula(
+            id, estado, fecha, alumno_id, cursodictado_id)
+    VALUES (1612,0,'24/02/13',1001,4010);
+
