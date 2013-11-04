43948457672f869d04c4f66eb9bd87311fa0dbf4
tephy <tephy@tephy-desktop.(none)>
2013-11-04 13:07:34 -0500

    issues #66 scritp insert carrera

---
+++ src/sql/carrera.sql
@@ -0,0 +1,21 @@
+﻿INSERT INTO carrera(
+            id, nombre)
+    VALUES (00198, 'Ciencia de la Computacion');
+INSERT INTO carrera(
+            id, nombre)
+    VALUES (00299, 'Administracion');
+INSERT INTO carrera(
+            id, nombre)
+    VALUES (003100, 'Educacion');
+INSERT INTO carrera(
+            id, nombre)
+    VALUES (004101, 'Psicologia');
+INSERT INTO carrera(
+            id, nombre)
+    VALUES (005102, 'Ingenieria Industrial');
+INSERT INTO carrera(
+            id, nombre)
+    VALUES (006103, 'Ingenieria en Telecomunicaciones');
+INSERT INTO carrera(
+            id, nombre)
+    VALUES (007105, 'Contabilidad');
