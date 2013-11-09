907b8d67608a86cbf1f41ad5d24ce893e9afe339
Jhoel Quispe <jhoel.quispe@ucsp.edu.pe>
2013-11-09 03:20:07 -0500

    script categoria_nota

---
+++ src/sql/categoria_nota.sql
@@ -0,0 +1,24 @@
+﻿INSERT INTO categoria_nota(
+            id, nombre)
+    VALUES (1201,'permanente1');
+INSERT INTO categoria_nota(
+            id, nombre)
+    VALUES (1202,'permanente2');
+INSERT INTO categoria_nota(
+            id, nombre)
+    VALUES (1203,'parcial');
+INSERT INTO categoria_nota(
+            id, nombre)
+    VALUES (1204,'final');
+INSERT INTO categoria_nota(
+            id, nombre)
+    VALUES (1205,'parcial teoria');
+INSERT INTO categoria_nota(
+            id, nombre)
+    VALUES (1206,'parcial practica');
+INSERT INTO categoria_nota(
+            id, nombre)
+    VALUES (1207,'final teoria');
+INSERT INTO categoria_nota(
+            id, nombre)
+    VALUES (1208,'final practica');
